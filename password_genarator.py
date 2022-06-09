37daa957c3ad053dae1bac57870c24b647cbfe71
Fazle Rabbi <94907550+rabbi303@users.noreply.github.com>
2022-06-09 10:45:26 +0600

    Update password_genarator.py

--- password_genarator.py
+++ password_genarator.py
@@ -1,18 +1,19 @@
-
+#import library
 import random
 
+#define variables
 numbers=['1','2','3','4','5','6','7','8','9']
 special_char=['!','@','#','$','%','&','*','(','{','[',']']
 char=['a','s','d','f','g','h','j','k','l','m','n','b','v','c','x','z','q','w','e','r','t','y','u','i','o','p','A','S','D','F','G','H','J','K','L','M','N','B','V','C','X','Z','Q','W','E','R','T','Y','U','I','O','P']
 
+#user inputs
+numbers_n=int(input('How many numbers you want in your password :'))
+special_char_n=int(input('How many special character you want in your password :'))
+char_n=int(input('How many latter you want in your password :'))
 
-
-numbers_n=int(input('How many numbers you want in your Passward :'))
-special_char_n=int(input('How many special character you want in your Passward :'))
-char_n=int(input('How many latter you want in your Passward :'))
-
+#variable for store password
 our_pass=[]
-
+#looping to pick randomized password from variables
 for nm in range(1,numbers_n+1):
     our_pass+=random.choice(numbers)
 
@@ -21,13 +22,14 @@ for sp in range(1,special_char_n+1):
 
 for cr in range(1,char_n+1):
     our_pass+=random.choice(char)
-
+#calling our_pass shuffly
 random.shuffle(our_pass)
-passward=" "
+password=" "
 
+#use for loop generate password as string
 for op in our_pass:
-    passward += op
-print('The strong passward is: ',passward)
+    password += op
+print('The strong password is: ',password)
 
 
 
